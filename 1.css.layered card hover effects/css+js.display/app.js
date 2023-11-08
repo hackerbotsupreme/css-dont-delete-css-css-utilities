206f9b8a9a280be670f5883f06337bbf1b3f0c67
Aloke Pramanik <129755834+hackerbotsupreme@users.noreply.github.com>
2023-11-08 10:04:14 +0530

    Add files via upload

---
+++ 1.css.layered card hover effects/css+js.display/app.js
@@ -0,0 +1,12 @@
+const articles = document.querySelectorAll(".container");
+const select = document.querySelector("select");
+
+function updateDisplay() {
+  articles.forEach((article) => {
+    article.style.display = select.value;
+  });
+}
+
+select.addEventListener("change", updateDisplay);
+
+updateDisplay();
