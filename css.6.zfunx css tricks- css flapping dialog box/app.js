1d0fc555fd3012723ee3f01fcef57b43eb105fb2
Aloke Pramanik <129755834+hackerbotsupreme@users.noreply.github.com>
2023-11-08 10:16:26 +0530

    Add files via upload

---
+++ css.6.zfunx css tricks- css flapping dialog box/app.js
@@ -0,0 +1,10 @@
+function showConfirmationNotification(id) {
+  document.getElementById(id).classList.add("show");
+}
+
+function hideConfirmationNotification(x) {
+  x.classList.remove("show");
+}
+function deleteSuccess() {
+  /*what to do after clicking yes*/
+}
